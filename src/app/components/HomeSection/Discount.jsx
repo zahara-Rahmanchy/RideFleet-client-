d3be2c644598ae2d2ace1fb0166009e6c0c554ae
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-08-30 19:27:32 +0600

    discount.jsx edited

--- src/app/components/HomeSection/Discount.jsx
+++ src/app/components/HomeSection/Discount.jsx
@@ -11,23 +11,25 @@ const Discount = () => {
     Aos.init({duration: 1000});
   }, []);
   return (
-    <section className="h-[400px] grid grid-cols-2 " style={{backgroundImage: `url(${"/brandon.jpg"})`,
+    <section className="min-h-[400px] my-32">
+      <section className="h-full grid grid-cols-2 " style={{backgroundImage: `url(${"/brandon.jpg"})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center"}}>
 
-        <div className="bg-sky-950 bg-opacity-80 h-[400px] flex flex-col justify-center items-center" data-aos="fade-up">
+        <div className=" md:col-span-1 col-span-2 bg-sky-950 bg-opacity-80 h-[400px] flex flex-col justify-center items-center mt-2 md:mt-0" data-aos="fade-up">
         <Image src="/students.png" width={90} height={24} alt="logo" />
             <h2 className="text-lg mb-3 text-white" data-aos="slide-up">Student</h2>
             <h3 className="text-2xl mb-3 text-yellow-200 font-semibold text-wrap" data-aos="slide-up"> Get 30% Discount</h3>
             <p className="text-blue-100 text-center w-1/2 text-wrap" data-aos="fade-up">On first booking,enter your Institute name and confirm with your identity card and phone number</p>
         </div>
-        <div className="bg-sky-950  bg-opacity-80 h-[400px] flex flex-col justify-center items-center" data-aos="slide-up">
+        <div className="md:col-span-1 col-span-2 bg-sky-950  bg-opacity-80 h-[400px] flex flex-col justify-center items-center" data-aos="slide-up">
          <FaUsers className="text-8xl text-blue-500" data-aos="slide-up"/>
          <h2 className="text-lg mb-3 text-white" data-aos="slide-up">Other Users</h2>
             <h2 className="text-2xl mb-3 text-yellow-200 font-semibold text-wrap" data-aos="slide-up"> Get 25% Discount </h2>
             <p className="text-blue-100 text-center w-1/2 text-wrap" data-aos="slide-up">For regular users, get 25% discount on your first booking</p>
         </div>
+    </section>
     </section>
   )
 }
