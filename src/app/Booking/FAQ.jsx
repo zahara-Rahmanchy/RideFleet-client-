13686b7697b88a6d03d9f842133f176f6b98b251
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-09-25 16:45:53 +0600

    error fixed

--- src/app/Booking/FAQ.jsx
+++ src/app/Booking/FAQ.jsx
@@ -157,8 +157,8 @@ const FAQ = () => {
           >
             <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
               <p className="mb-2 text-gray-500 dark:text-gray-400">
-                To rent a vehicle, you must have a valid driver's license and
-                meet the minimum age requirements specified for each vehicle
+                To rent a vehicle, you must have a valid driver&apos;s license
+                and meet the minimum age requirements specified for each vehicle
                 type. Additionally, you may need to provide a security deposit
                 and proof of insurance, depending on the vehicle.
               </p>
